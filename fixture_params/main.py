6fe27440f32f06d6ebcb2b972f42e97a4e5aa7cc
Zdenek Kraus <zdenek.kraus@gmail.com>
2025-10-26 13:46:28 +0100

    DOC(fixture_params) adding docstrings

--- fixture_params/main.py
+++ fixture_params/main.py
@@ -1,9 +1,19 @@
+"""
+Definition of SUT Software under test.
+"""
+
+
 class Sut:
+    """Model for SUT"""
+
     def __init__(self, a):
+        """Create SUT with data specified by a"""
         self.a = a
 
     def get(self):
+        """Return available data"""
         return self.a
 
     def get_prefix(self, prefix):
+        """Get processed prefixed data"""
         return f"{prefix}{self.a}"
